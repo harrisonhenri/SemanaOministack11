51982db8c62c6d0352e07c53a598086f809e0d88
harrisonhenri <harrisonhenrisn@gmail.com>
2020-03-26 23:00:10 -0300

    Aplicação Mobile finalizada

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center">
+  <img src="./frontend/src/assets/logo.svg">
+</p>
+
+# SemanaOministack11
+
+Repositório contendo as atividades do curso da Semana Oministack 11.
--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,12 +2,12 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index(req, res) {
-    const { page = 1 } = req.body;
+    const { page = 1 } = req.query;
 
     const [count] = await connection('incidents').count();
 
     const incidents = await connection('incidents')
-      .join('ongs', 'ongs.id', '=', 'incidents.ongs_id')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
       .select([
